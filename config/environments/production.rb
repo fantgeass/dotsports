b9b6b830bcfb10db21dae39c42a2b803899fe911
Denis I. Lukyanov <d.lukyanov@secoint.ru>
2012-11-20 19:42:08 +0400

    Enabled memcached as cache store.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Dotsports::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store, '127.0.0.1', { :namespace => "dotsports-001", :expires_in => 1.day }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
