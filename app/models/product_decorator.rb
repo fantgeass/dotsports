3b73ffd76ed6fcd1e7790d7028fceba14a62282f
Marat Islamov <pitabrus@gmail.com>
2012-12-17 11:54:00 +0400

    Attr_accessible removed, bug fixed

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -2,7 +2,6 @@ Product.class_eval do
   has_one :product_extension
   accepts_nested_attributes_for :product_extension, :allow_destroy => true
   delegate_belongs_to :product_extension, :old_price, :embedded_video
-  attr_accessible :old_price, :embedded_video
 
   def meta_description
     taxon = taxons.order(:permalink).where('permalink like "cat/%"').first || taxons.first
--- app/models/product_extension.rb
+++ app/models/product_extension.rb
@@ -1,4 +1,3 @@
 class ProductExtension < ActiveRecord::Base
   belongs_to :product, :class_name => "Product"
-  attr_accessible :old_price
 end
